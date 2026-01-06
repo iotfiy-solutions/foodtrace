ef28882fc4971ea0e7339de69b260e2d15c1eccc
FaRaZ-32729 <sial32729@gmail.com>
2026-01-06 15:27:26 +0500

    updated backend

--- backend/src/config/firebaseAdmin.js
+++ backend/src/config/firebaseAdmin.js
@@ -1,11 +1,6 @@
 const admin = require("firebase-admin");
 const dotenv = require("dotenv").config();
-// const serviceAccount = require("./firebaseKey.json");
 
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//     storageBucket: "frost-control-97eb3.firebasestorage.app",
-// });
 admin.initializeApp({
     credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
