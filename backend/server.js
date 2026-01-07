51da79122d0e1ddf56e9ff73262bc999397f8cf0
Muhammad-Umar-Waqar <mohammthegreat@gmail.com>
2026-01-07 18:43:17 +0500

    adding code

--- backend/server.js
+++ backend/server.js
@@ -54,6 +54,9 @@ app.use(
     express.static(path.join(__dirname, "./assets"))
 );
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 
 
 // Routes
